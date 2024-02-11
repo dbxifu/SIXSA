aa69c4ff871f35a990e6d2cd9ee76da992645e8b
Didier BARRET <didier.barret@irap.omp.eu>
2024-02-11 09:44:07 +0100

    Initial commit

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Welcome to SIXSA')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
